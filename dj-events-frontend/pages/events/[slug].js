a91e9075caadc0c96e09625c87c36f1ea59a9d8f
ujwal-danta <ujwaldanta@gmail.com>
2022-07-18 08:12:19 +0530

    event page

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,5 +1,26 @@
-export default function EventPage() {
+import { API_URL } from "@/config/index"
+export default function EventPage({ post }) {
+    console.log(post)
     return (
-        <div>slug</div>
+        <div>{post.name}</div>
     )
+}
+
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+    console.log(events)
+    const paths = events.map(evt => ({
+        params: { slug: evt.slug }
+    }))
+    return { paths, fallback: true }
+}
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(`${API_URL}/api/events/${params.slug}`)
+    const post = await res.json()
+
+    // Pass post data to the page via props
+    return { props: { post: post[0] } }
 }
--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,21 +1,32 @@
 import Layout from "@/components/Layout"
 import { API_URL } from '@/config/index'
-export default function HomePage({ posts }) {
-  console.log(posts)
+import EventItem from "@/components/EventItem"
+import Link from 'next/link'
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
 
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`)
-  const posts = await res.json()
+  const events = await res.json()
 
   return {
     props: {
-      posts,
+      events: events.slice(0, 3),
     },
     revalidate: 1,
   }
