6e8d47b73b4374a3abf02951a64256ef077cfec4
ujwal-danta <ujwaldanta@gmail.com>
2022-07-19 17:23:28 +0530

    front-end done

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,8 +1,51 @@
 import { API_URL } from "@/config/index"
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '@/styles/Event.module.css'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Layout from "@/components/Layout"
 export default function EventPage({ post }) {
-    console.log(post)
+    const deleteEvent = (e) => {
+        console.log('delete')
+    }
     return (
-        <div>{post.name}</div>
+        <Layout>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/events/edit/${post.id}`}>
+                        <a>
+                            <FaPencilAlt /> Edit Event
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete}
+                        onClick={deleteEvent}
+                    >
+                        <FaTimes /> Delete Event
+                    </a>
+                </div>
+                <span>
+                    {post.date} at {post.time}
+                </span>
+                <h1>{post.name}</h1>
+                {post.image && (
+                    <div className={styles.image}>
+                        <Image src={post.image} width={960} height={600} />
+                    </div>
+                )}
+                <h3>Peformers:</h3>
+                <p>{post.peformers}</p>
+                <h3>Description:</h3>
+                <p>{post.description}</p>
+                <h3>Venue: {post.venue}</h3>
+                <p>{post.address}</p>
+
+                <Link href={`/events`}>
+                    <a className={styles.back}>
+                        {"<"}   Go Back
+                    </a>
+                </Link>
+            </div>
+        </Layout>
     )
 }
 
